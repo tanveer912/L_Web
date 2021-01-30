584dac48c2f2ab408ad11b83ca140f66daea5748
Tanveer <khansamson1@gmail.com>
2021-01-30 23:11:31 +0600

    nav and database created

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,22 +1,26 @@
-<nav class="navbar navbar-inverse">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+ 
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
-  <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+  <div class="collapse navbar-collapse" id="navbarsExample03">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="dropdown-item" href="https://localhost/L_Web/public/index">Home</a>
+      <li class="nav-item ">
+        <a class="nav-link active" href="https://localhost/L_Web/public/index">Home </a>
       </li>
       <li class="nav-item">
-        <a class="dropdown-item" href="https://localhost/L_Web/public/about">About</a>
+        <a class="nav-link active" href="https://localhost/L_Web/public/about">About</a>
       </li>
-      
-      
-        </div>
+      <li class="nav-item">
+        <a class="nav-link disabled" href=>Not Active</a>
       </li>
+     
     </ul>
- 
+    <form class="form-inline my-2 my-md-0">
+      <input class="form-control" type="text" placeholder="Search">
+    </form>
   </div>
 </nav>
